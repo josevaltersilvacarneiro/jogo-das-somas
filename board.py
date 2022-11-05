de8f09f3b737f050e534cc4a75da60a9b0371def
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-11-05 12:24:53 -0300

    The Board class was tested. Some errors has been identified.

--- board.py
+++ board.py
@@ -13,3 +13,26 @@ class Board:
     def __getitem__(self, index : int) -> Section:
 
         return self._board.__getitem__(index)
+
+if __name__ == '__main__':
+
+    # Testing the Board class
+
+    # Available methods using dir
+
+    print(dir(Board))
+
+    for i in range(-100, 101):
+
+        # Initializing the Board class
+
+        init = Board(i)
+
+        # Number of input, object repr, methods
+        print(i, init)
+
+        # Errors
+
+        # If the method __getitem__ receives as parameter a index greater or smaller 
+        # than the length of board object, it returns a error
+        # Output: IndexError: list index out of range
