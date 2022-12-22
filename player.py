9e91523fc5c7d90c68e58e67bfc3c431e963d24f
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-12-22 10:46:44 -0300

    A bug was fixed

--- player.py
+++ player.py
@@ -12,10 +12,10 @@ class Player:
         self._NAME : str = name
         self._score : int = 0
 
-    def __eq__(self, player : Player) -> bool:
+    def __eq__(self, player : object) -> bool:
         return self._score == player.score
 
-    def __lt__(self, player : Player) -> bool:
+    def __lt__(self, player : object) -> bool:
         return self._score < player.score;
 
     # public
