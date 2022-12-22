8b407b089a33f4992cfb1f0f63c92ec5d5f1144f
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-12-22 11:15:16 -0300

    New comments were added to detail how access and modify the attributes of Player instances

--- player.py
+++ player.py
@@ -2,9 +2,27 @@ from functools import total_ordering
 
 @total_ordering
 class Player:
+    """When instantiating a new Player object,
+    the player's name must be passed as argu-
+    ment. For example:
 
-    """
-        This class defines all the actions and features of the player
+    new_player = Player('John');
+
+    The name and score attributes are publicly
+    available. To access them, use the methods
+    below:
+
+    new_player_name  = new_player.name;
+    new_player_score = new_player.score;
+
+    The player's score can be changed. However,
+    the  value will be incremented, i.e. it's
+    not possible to decrease it. See below:
+
+    print(new_player.score); # 5
+    new_player.score = 5;
+    print(new_player.score); # 10
+    
     """
 
     def __init__(self, name : str):
